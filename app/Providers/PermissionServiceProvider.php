3cb6f7fecdf3a5d9b0935079f36071b670affd00
Unknown <eddzzy@gmail.com>
2018-01-28 22:45:14 +0300

    Set the permissions to only be fetched in this provider if the permissions table exists

--- app/Providers/PermissionServiceProvider.php
+++ app/Providers/PermissionServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\ServiceProvider;
 use App\Models\Permission;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Schema;
 
 class PermissionServiceProvider extends ServiceProvider
 {
@@ -16,11 +17,14 @@ class PermissionServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Permission::get()->map( function ($permission) {
-            Gate::define($permission->name, function ($user) use ($permission) {
-                return $user->hasPermissionTo($permission);
+        if (Schema::hasTable('permissions')) {
+            Permission::get()->map( function ($permission) {
+                Gate::define($permission->name, function ($user) use ($permission) {
+                    return $user->hasPermissionTo($permission);
+                });
             });
-        });
+        }
+
 
         Blade::directive('role', function ($role) {
             return "<?php if (auth()->check() && auth()->user()->hasRole({$role})): ?>";
