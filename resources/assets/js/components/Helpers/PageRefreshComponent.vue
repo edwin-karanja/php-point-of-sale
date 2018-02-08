fdb848f0848d30ddddf602364c55a54a36db95ae
Unknown <eddzzy@gmail.com>
2018-02-08 16:50:53 +0300

    Worked on a page refresh component to prevent auto logout when session expires

---
+++ resources/assets/js/components/Helpers/PageRefreshComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="hidden">
+        Page refresh component
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                durationInMinutes: 15
+            };
+        },
+
+        methods: {
+            keepTokenAlive () {
+                axios.post('/keep-token-alive').then((response) => {
+                    console.log('kept alive..') //@TODO remove this on going to production
+                })
+            }
+        },
+
+        mounted() {
+            setInterval(() => {
+                this.keepTokenAlive()
+            }, 1000 * 60 * this.durationInMinutes)
+        }
+    }
+</script>
+
+<style scoped>
+    .hidden {
+        display: none;
+    }
+</style>
